33e8fed293fef0bb33b87d62b7aa1d523ab85481
Ritwik <ritwikkumarrishu@gmail.com>
2023-07-31 20:49:35 +0530

    Images uploaded in aws

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,19 +10,28 @@ function DemoCarousel() {
       <Carousel>
         <Link to={"/productPage/63b7c5bb960520d93f84c9b8"}>
           <div className="image" key={1}>
-            <img src="/images/airpods.jpg" alt="ps4" />
+            <img
+              src="https://ritwikproshop.s3.ap-south-1.amazonaws.com/playstation.jpg"
+              alt="ps4"
+            />
             <p className="legend">Legend 1</p>
           </div>
         </Link>
         <Link to={"/productPage/63b7c5bb960520d93f84c9bd"}>
           <div className="image">
-            <img src="/images/alexa.jpg" alt="ps4 pro" />
+            <img
+              src="https://ritwikproshop.s3.ap-south-1.amazonaws.com/alexa.jpg"
+              alt="alexa"
+            />
             <p className="legend">Legend 2</p>
           </div>
         </Link>
         <Link to={"/productPage/63b7c5bb960520d93f84c9ba"}>
           <div className="image">
-            <img src="/images/camera.jpg" alt="ps4 pro2" />
+            <img
+              src="https://ritwikproshop.s3.ap-south-1.amazonaws.com/camera.jpg"
+              alt="camera"
+            />
             <p className="legend">Legend 3</p>
           </div>
         </Link>
