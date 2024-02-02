7548677e688271accf123ba6beae91a655ab3404
Ritwik <ritwikkumarrishu@gmail.com>
2024-02-02 13:47:13 +0530

    Converting userSlice.js from javascript to typescript - part 1

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,26 +1,118 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import BASE_URL from "../../helper/url";
+
+// Types
+interface UserState {
+  isLoading: boolean;
+  data: UserData | null;
+  isError: boolean;
+  errorMsg: string;
+  updationComplete: boolean;
+  emailSent: boolean;
+  passwordReseted: boolean;
+  jwtExpired: boolean;
+}
+
+interface UserData {
+  token: string;
+  user: {
+    _id: string;
+    email: string;
+    name: string;
+    address: Array<{
+      name: string;
+      phoneNumber: string;
+      address: string;
+      city: string;
+      state: string;
+      postal: number;
+      country: string;
+      _id: string;
+    }>;
+  };
+}
+
+interface CreateSessionThunkArgs {
+  sessionType: string;
+  userData: {
+    name?: string;
+    email: string;
+    password: string;
+    confirmPassword?: string;
+  };
+}
+
+interface UpdateUserThunkArgs {
+  // Add fields of profile page
+  id: string;
+}
+
+interface AddUserAddressThunkArgs {
+  userAddress: {
+    address: string;
+    city: string;
+    postal_code: string;
+    country: string;
+  };
+  userId: string;
+}
+
+interface DeleteUserAddressThunkArgs {
+  userId: string;
+  addressId: string;
+}
+
+interface UpdatePasswordThunkArgs {
+  currPassword: string;
+  newPassword: string;
+  renewPassword: string;
+  id: string;
+}
+
+interface ForgotPasswordThunkArgs {
+  email: string;
+}
+
+interface ResetPasswordThunkArgs {
+  password: {
+    newPassword: string;
+    confirmPassword: string;
+  };
+  token: string;
+}
+
+interface CheckJWTExpiryThunkArgs {
+  token: string;
+}
+
+// interface ApiError {
+//   error: string;
+//   // other properties if applicable
+// }
 
 // Action
 export const createSession = createAsyncThunk(
   "createSession",
-  async ({ sessionType, userData }, { rejectWithValue }) => {
+  async (
+    { sessionType, userData }: CreateSessionThunkArgs,
+    { rejectWithValue }
+  ) => {
     try {
       const response = await axios.post(
         `${BASE_URL}/api/v1/user/${sessionType}`,
         userData
       );
-      return response.data;
+      return response.data as { user: object; token: string };
     } catch (error) {
-      return rejectWithValue(error.response.data);
+      return rejectWithValue((error as AxiosError).response?.data);
     }
   }
 );
 
 export const addUserAddress = createAsyncThunk(
   "addUserAddress",
-  async (body) => {
+  async (body: AddUserAddressThunkArgs) => {
     try {
       const response = await axios.patch(
         `${BASE_URL}/api/v1/user/addAddress`,
@@ -35,7 +127,7 @@ export const addUserAddress = createAsyncThunk(
 
 export const deleteUserAddress = createAsyncThunk(
   "deleteUserAddress",
-  async (body) => {
+  async (body: DeleteUserAddressThunkArgs) => {
     try {
       const { userId, addressId } = body;
       const { data } = await axios.delete(
@@ -48,14 +140,17 @@ export const deleteUserAddress = createAsyncThunk(
   }
 );
 
-export const updateUser = createAsyncThunk("updateUser", async (body) => {
-  const response = await axios.put(`${BASE_URL}/api/v1/user/update`, body);
-  return response.data;
-});
+export const updateUser = createAsyncThunk(
+  "updateUser",
+  async (body: UpdateUserThunkArgs) => {
+    const response = await axios.put(`${BASE_URL}/api/v1/user/update`, body);
+    return response.data;
+  }
+);
 
 export const updatePassword = createAsyncThunk(
   "updatePassword",
-  async (body, { rejectWithValue }) => {
+  async (body: UpdatePasswordThunkArgs, { rejectWithValue }) => {
     try {
       const { data } = await axios.put(
         `${BASE_URL}/api/v1/user/updatePassword`,
@@ -63,14 +158,16 @@ export const updatePassword = createAsyncThunk(
       );
       return data;
     } catch (error) {
-      return rejectWithValue(error.response.data.message);
+      // TODO: Can't dispatch message from here (error.response.data.message)
+      const errorMessage = (error as AxiosError)?.response?.data;
+      return rejectWithValue(errorMessage);
     }
   }
 );
 
 export const forgotPassword = createAsyncThunk(
   "forgotPassword",
-  async (user, { rejectWithValue }) => {
+  async (user: ForgotPasswordThunkArgs, { rejectWithValue }) => {
     try {
       const { data } = await axios.post(
         `${BASE_URL}/api/v1/user/forgotPassword`,
@@ -78,32 +175,35 @@ export const forgotPassword = createAsyncThunk(
       );
       return data;
     } catch (error) {
-      return rejectWithValue(error.response.data);
+      return rejectWithValue((error as AxiosError).response?.data);
     }
   }
 );
 
-export const resetPassword = createAsyncThunk("resetPassword", async (body) => {
-  try {
-    const axiosInstanceWithJWT = axios.create({
-      headers: {
-        Authorization: `Bearer ${body?.token}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const { data } = await axiosInstanceWithJWT.patch(
-      `${BASE_URL}/api/v1/user/resetPassword`,
-      body?.password
-    );
-    return data;
-  } catch (error) {
-    console.log(error);
+export const resetPassword = createAsyncThunk(
+  "resetPassword",
+  async (body: ResetPasswordThunkArgs) => {
+    try {
+      const axiosInstanceWithJWT = axios.create({
+        headers: {
+          Authorization: `Bearer ${body?.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const { data } = await axiosInstanceWithJWT.patch(
+        `${BASE_URL}/api/v1/user/resetPassword`,
+        body?.password
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 export const checkJWTExpiry = createAsyncThunk(
   "checkJWTExpiry",
-  async ({ token }, { rejectWithValue }) => {
+  async ({ token }: CheckJWTExpiryThunkArgs, { rejectWithValue }) => {
     try {
       const axiosInstanceWithJWT = axios.create({
         headers: {
@@ -116,7 +216,7 @@ export const checkJWTExpiry = createAsyncThunk(
       );
       return data;
     } catch (error) {
-      return rejectWithValue(error.response.data);
+      return rejectWithValue((error as AxiosError).response?.data);
     }
   }
 );
@@ -126,7 +226,7 @@ const userSlice = createSlice({
   initialState: {
     isLoading: false,
     data: localStorage.getItem("user")
-      ? JSON.parse(localStorage.getItem("user"))
+      ? JSON.parse(localStorage.getItem("user") as string)
       : null,
     isError: false,
     errorMsg: "",
@@ -134,15 +234,15 @@ const userSlice = createSlice({
     emailSent: false,
     passwordReseted: false,
     jwtExpired: false,
-  },
+  } as UserState,
   reducers: {
-    logOut: (state, action) => {
+    logOut: (state) => {
       state.data = null;
       localStorage.removeItem("user");
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(createSession.pending, (state, action) => {
+    builder.addCase(createSession.pending, (state) => {
       state.isLoading = true;
       state.data = null;
       state.isError = false;
@@ -156,10 +256,11 @@ const userSlice = createSlice({
     builder.addCase(createSession.rejected, (state, action) => {
       state.isLoading = false;
       state.data = null;
-      state.isError = action.payload.error;
+      state.errorMsg = action.payload.error;
+      state.isError = true;
     });
 
-    builder.addCase(addUserAddress.pending, (state, action) => {
+    builder.addCase(addUserAddress.pending, (state) => {
       state.isLoading = true;
     });
     builder.addCase(addUserAddress.fulfilled, (state, action) => {
@@ -167,10 +268,10 @@ const userSlice = createSlice({
       state.data = action.payload;
       localStorage.setItem("user", JSON.stringify(state.data));
     });
-    builder.addCase(addUserAddress.rejected, (state, action) => {});
+    builder.addCase(addUserAddress.rejected, () => {});
 
     // updateUser
-    builder.addCase(updateUser.pending, (state, action) => {
+    builder.addCase(updateUser.pending, (state) => {
       state.isLoading = true;
     });
     builder.addCase(updateUser.fulfilled, (state, action) => {
@@ -185,7 +286,7 @@ const userSlice = createSlice({
     });
 
     // update password
-    builder.addCase(updatePassword.pending, (state, action) => {
+    builder.addCase(updatePassword.pending, (state) => {
       state.isLoading = true;
     });
     builder.addCase(updatePassword.fulfilled, (state, action) => {
@@ -212,30 +313,30 @@ const userSlice = createSlice({
     });
 
     // Forgot Password
-    builder.addCase(forgotPassword.pending, (state, action) => {
+    builder.addCase(forgotPassword.pending, (state) => {
       state.isLoading = true;
       state.emailSent = false;
       state.isError = false;
     });
-    builder.addCase(forgotPassword.fulfilled, (state, action) => {
+    builder.addCase(forgotPassword.fulfilled, (state) => {
       state.isLoading = false;
       state.emailSent = true;
     });
-    builder.addCase(forgotPassword.rejected, (state, action) => {
+    builder.addCase(forgotPassword.rejected, (state) => {
       state.isLoading = false;
       state.isError = true;
       state.emailSent = false;
     });
 
     // Add Case
-    builder.addCase(resetPassword.pending, (state, action) => {
+    builder.addCase(resetPassword.pending, (state) => {
       state.isLoading = true;
     });
-    builder.addCase(resetPassword.fulfilled, (state, action) => {
+    builder.addCase(resetPassword.fulfilled, (state) => {
       state.isLoading = false;
       state.passwordReseted = true;
     });
-    builder.addCase(resetPassword.rejected, (state, action) => {
+    builder.addCase(resetPassword.rejected, (state) => {
       state.isLoading = false;
       state.isError = true;
       state.passwordReseted = false;
---
+++ type-definitions.d.ts
@@ -0,0 +1,4 @@
+// declare module "react-router-dom";
+
+// declare module "src/wrapper/HeaderWrapper.js";
+declare module "src/store/slices/userSlice.js";
